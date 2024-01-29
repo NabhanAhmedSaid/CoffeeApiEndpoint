51f1af259f32a750188f64bc29e3db1c89b6f856
safcsp dev <dev@test.com>
2024-01-29 21:23:37 +0400

    Add project files.

---
+++ CoffeeApiEndpoint/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+string[] drinks = { "Mocha","Black Coffee","Tukish Coffe" };
+WebApplicationBuilder wepAppBuilder = WebApplication.CreateBuilder(args);
+
+WebApplication  webApp = wepAppBuilder.Build();
+
+
+webApp.MapGet("api/info", (Func<string>)(() =>
+    "This is C# endpoint "));
+
+webApp.MapGet("api/drinks", (Func<string[]>)(()=>drinks));
+webApp.MapGet("api/drinks/1", (Func<string>)(() => drinks[0]));
+webApp.MapPost("api/year", (Func<int>)(() => 2023));
+
+webApp.Run();
